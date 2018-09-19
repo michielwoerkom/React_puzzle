75f797c8bee4640485d79e668dd12c8bda35b905
Michiel van Woerkom <michielwoerkom@gmail.com>
2018-09-19 16:35:52 +0200

    Puzzel v1

---
+++ js/aftermath.js
@@ -0,0 +1,13 @@
+
+
+/*
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+   alert('dn deze');
+   var get_tiles = document.getElementsByClassName("tile");
+   for (i=0; i < get_tiles.lenght; i++;) {
+   console.log("gevonden");
+   };
+})();
+*/
